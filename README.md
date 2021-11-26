24229d9366afc60ff68278408cb34ebb7d8eb154
Gnv Saikiran <gajjarapusai1@gmail.com>
2021-11-27 02:31:23 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Naruto Gifs
+
+This project uses React for frontend. This Dapp runs on Solana blockchain. The Solana program is written using Rust.
+
+You can submit your favourite gifs and other users can see them by connecting their solana wallet to the web app. All the users will be able to see the public address of the user who submitted the gif.
+
+[You can found the frontend code here](https://replit.com/@gnv24/gif-portal-starter-project)
---
+++ tests/myepicproject.js
@@ -0,0 +1,52 @@
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
+
+const main = async () => {
+  console.log("🚀 Starting test...");
+
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Myepicproject;
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+
+  console.log("📝 Your transcation signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  await program.rpc.addGif(
+    "https://media4.giphy.com/media/maINkm7zjcL4s/giphy.gif?cid=790b761178d2cbc5ea591b945af39def645b60d6b2eb55fe&rid=giphy.gif&ct=g",
+    {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    }
+  );
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+  console.log("👀 GIF List", account.gifList);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
